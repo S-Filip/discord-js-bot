d6465dca0caf34ca42330a7ee408fde8729f8a52
S-Filip <100999946+S-Filip@users.noreply.github.com>
2022-12-31 00:57:20 +0100

    Reverting ping changes

--- src/commands/Community/ping.js
+++ src/commands/Community/ping.js
@@ -3,6 +3,6 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder().setName("ping").setDescription(`Replies with "pong".`),
   async execute(interaction) {
-    await interaction.reply({ content: `pong. 🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms); ` });
+    await interaction.reply({ content: `pong` });
   },
 };
